d51b0fcc1a138cc365b8711b805d3401216d420b
PSCompSci <pstacey@deakin.edu.au>
2021-08-14 12:35:58 +1000

    Started backend server code

--- vmmm/main.go
+++ vmmm/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+type server struct {
+	router *mux.Router
+}
+
+func (s *server) routes() {
+	s.router.HandleFunc("/", s.handleIndex())
+}
+
+func (s *server) handleIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+	s := server{router: r}
+	s.routes()
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	log.Fatal(http.ListenAndServe(":3001", handlers.CORS(originsOk, headersOk, methodsOk)(s.router)))
 }
